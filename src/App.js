5c878613e1bab442f1a027ece4c42a2c15db1024
omrsfylmz <omrsfylmz@gmail.com>
2021-04-17 01:59:36 +0300

    React recipe app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import "./App.css";
-import Card from "./components/Card/Card";
-import food from "./assets/food.jpg"; // Tell webpack this JS file uses this image
+import './App.css';
+import Card from './components/Card/Card';
+import food from './assets/food.jpg'; // Tell webpack this JS file uses this image
 
 function App() {
-  const recipeAuthor = "Kodluyoruz";
+  const recipeAuthor = 'Kodluyoruz';
   const recipeItem = {
-    title: "Avokado Ezmeli Taco",
-    date: "12 Ocak 2021, Salı",
+    title: 'Avokado Ezmeli Taco',
+    date: '12 Ocak 2021, Salı',
     image: food,
     description:
       "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
   };
 
   const likeCount = 193;
-  const isLiked = false;
+  const isLiked = true;
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <Card
           /* prop ismi = { değişken } */
           author={recipeAuthor}
+          recipeItem={recipeItem}
+          likeCount={likeCount}
+          liked={isLiked}
         />
       </header>
     </div>
